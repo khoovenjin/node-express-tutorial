0d31f944241da53448a9ab6efd0bdcd1ea1ae40d
troberliner3 <62326948+troberliner3@users.noreply.github.com>
2021-08-19 13:48:06 +0800

    json express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,21 +1,11 @@
 const express = require('express')
-const app = express();
-const path = require('path')
+const app = express()
+const {products} = require('data.js')
 
-// set up static and middleware
-app.use(express.static('./public'))
-
-// This app get has 2 options:
-// 1) put in static assets like above : but its better to dp it this way
-// 2) Server Side Rendering by using template engines
 app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
-
-app.all('*', (req, res)=>{
-    res.status(404).send('resource not found')
+    res.json(products)
 })
 
 app.listen(5000, () =>{
-    console.log('server is listening on port 5000...');
+    console.log('Server is listening on port 5000...');
 })
--- 2-express-tutorial/final/3-express-basics.js
+++ 2-express-tutorial/final/3-express-basics.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get(('/'), (req, res) => {
+app.get('/', (req, res) => {
     console.log('user hit the resource');
     res.status(200).send('Home Page')
 })
 
-app.get(('/about'), (req, res) => {
+app.get('/about', (req, res) => {
     res.status(200).send('About Page')
 })
 
